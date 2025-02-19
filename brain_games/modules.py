f7c8dbed8d0739f54e27642f530c893090f7f080
DaniilShomin <d.sh.v@yandex.ru>
2025-02-19 21:24:41 +0300

    add new module gcd

--- brain_games/modules.py
+++ brain_games/modules.py
@@ -38,8 +38,8 @@ def parity_check(name):
         elif parity != 0 and answer != 'no':
             was_correct_answer(False, name, answer, 'no')
             break
-        if question_num == 3:
-            was_correct_answer(True, name)
+    if question_num == 3:
+        was_correct_answer(True, name)
 
 
 def calc(name):
@@ -65,5 +65,34 @@ def calc(name):
         else:
             was_correct_answer(False, name, answer, str(result))
             break
-        if question_num == 3:
-            was_correct_answer(True, name)
+    if question_num == 3:
+        was_correct_answer(True, name)
+
+
+def gcd(name):
+    print("Find the greatest common divisor of given numbers.")
+    question_num = 0
+    while question_num < 3:
+        number_one = randint(1, 99)
+        number_two = randint(1, 99)
+        print(f"Question: {number_one} {number_two}")
+        answer = input("Your answer: ")
+        if number_one < number_two:
+            number_one, number_two = number_two, number_one
+        if number_one % number_two != 0:
+            for result in range(int(number_two / 2), 0, -1):
+                if number_one % result == 0 and number_two % result == 0:
+                    break
+            if int(answer) == result:
+                print('Correct!')
+                question_num += 1
+            else:
+                was_correct_answer(False, name, answer, str(result))
+                break
+        else:
+            result = number_two
+            if int(answer) == result:
+                print('Correct!')
+                question_num += 1
+    if question_num == 3:
+        was_correct_answer(True, name)
