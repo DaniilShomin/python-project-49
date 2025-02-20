8df48bc79b36f1e45813afd4794d0161bb0c64ad
DaniilShomin <d.sh.v@yandex.ru>
2025-02-20 21:42:23 +0300

    fix addiction

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
-from brain_games.games.even import even
+from brain_games.games.even import parity_check
 from brain_games.modules import welcome_user
 
 
 def main():
     name = welcome_user()  
-    even(name)
+    parity_check(name)
 
 
 if __name__ == '__main__':
