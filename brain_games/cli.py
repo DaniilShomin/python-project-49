7bce9589c2a7dd6b7728f10ae2ab9371f39489fd
ShominDaniil <d.sh.v@yandex.ru>
2025-11-22 00:46:32 +0300

    fix: typo

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,7 +2,7 @@ import prompt  # type: ignore
 
 
 def welcome_user() -> str:
-    print("Welcome to the Brain Game!")
+    print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}")
     return name
