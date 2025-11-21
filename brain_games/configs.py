f08a0fa3d6959921b545806a192fe5ac5c442747
ShominDaniil <d.sh.v@yandex.ru>
2025-11-22 00:53:39 +0300

    fix: edit rules

--- brain_games/configs.py
+++ brain_games/configs.py
@@ -5,8 +5,9 @@ RULES = {
     "brain-calc": "What is the result of the expression?",
     "brain-gcd": "Find the greatest common divisor of given numbers.",
     "brain-progression": "What number is missing in the progression?",
-    "brain-prime": """Answer "yes" if given number is prime. 
-                    Otherwise answer "no".""",
+    "brain-prime": (
+        'Answer "yes" if given number is prime. Otherwise answer "no".'
+    ),
 }
 
 # min max values for gas
