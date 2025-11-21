b3c08c174410629eabca868d934f223bc7e67a5f
ShominDaniil <d.sh.v@yandex.ru>
2025-11-22 00:48:55 +0300

    fix: typo

--- brain_games/configs.py
+++ brain_games/configs.py
@@ -2,7 +2,7 @@ MAX_GAME_COUNT = 3
 
 RULES = {
     "brain-even": 'Answer "yes" if the number is even, otherwise answer "no".',
-    "brain-calc": "What is the result of the expresion?",
+    "brain-calc": "What is the result of the expression?",
     "brain-gcd": "Find the greatest common divisor of given numbers.",
     "brain-progression": "What number is missing in the progression?",
     "brain-prime": """Answer "yes" if given number is prime. 
